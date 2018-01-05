5bba44e5d72d87c0e76bc4526382033cf0d38055
JoSuzuki <jonathan@suzuki.pro.br>
2018-01-05 15:46:53 -0200

    Typescript setup

--- LoginAppProject/android/app/src/main/java/com/loginappproject/MainApplication.java
+++ LoginAppProject/android/app/src/main/java/com/loginappproject/MainApplication.java
@@ -15,6 +15,10 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
+    protected String getJSMainModuleName() {
+      return "artifacts/index.android";
+    }
+    @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
--- LoginAppProject/src/App.tsx
+++ LoginAppProject/src/App.tsx
@@ -14,7 +14,7 @@ import {
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+    'Cmd+D or shake for dev menu holy that was hard',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
