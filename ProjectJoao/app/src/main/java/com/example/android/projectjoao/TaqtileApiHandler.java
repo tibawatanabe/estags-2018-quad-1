c2fe42ebaaa756c92a4b8af31d829a5383e86bb8
joaoCasella <jpedrocaselladepaula@hotmail.com>
2018-01-11 19:31:49 -0200

    Added the request of the new actions

--- ProjectJoao/app/src/main/java/com/example/android/projectjoao/TaqtileApiHandler.java
+++ ProjectJoao/app/src/main/java/com/example/android/projectjoao/TaqtileApiHandler.java
@@ -10,9 +10,11 @@ import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
@@ -30,4 +32,10 @@ public interface TaqtileApiHandler {
 
     @POST("user")
     Call<CreateResponse> createUser(@Header("Authorization") String token, @Body User user);
+
+    @DELETE("user/{id}")
+    Call<CreateResponse> deleteUser(@Header("Authorization") String token, @Path("id") int id);
+
+    @PUT("user/{id}")
+    Call<CreateResponse> editUser(@Header("Authorization") String token, @Path("id") int id, @Body User user);
 }
