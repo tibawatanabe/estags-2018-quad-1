711f831bee4a9ff0e41c10ba5210fddfe3a45e47
joaoCasella <jpedrocaselladepaula@hotmail.com>
2018-01-11 19:30:42 -0200

    Added the actions of deleting and editing the users

--- ProjectJoao/app/src/main/java/com/example/android/projectjoao/ShowActivity.java
+++ ProjectJoao/app/src/main/java/com/example/android/projectjoao/ShowActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.projectjoao;
 
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.projectjoao.model.CreateResponse;
 import com.example.android.projectjoao.model.ShowResponse;
 import com.example.android.projectjoao.model.User;
 
@@ -25,6 +31,8 @@ public class ShowActivity extends AppCompatActivity {
     private TextView mRoleTextView;
     private TextView mCreatedAtTextView;
     private TextView mUpdatedAtTextView;
+    private SharedPreferences pref;
+    private DialogFragment newFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +49,7 @@ public class ShowActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
 
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("SharedPreferences", 0); // 0 - for private mode
+        pref = getApplicationContext().getSharedPreferences("SharedPreferences", 0); // 0 - for private mode
 
         apiHandler = NetworkConnection.getConnection();
 
@@ -75,6 +83,67 @@ public class ShowActivity extends AppCompatActivity {
         @Override
         public void onFailure(Call<ShowResponse> call, Throwable t) {
             Toast.makeText(getApplicationContext(), "Erro na conexão", Toast.LENGTH_SHORT).show();
+            t.printStackTrace();
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edit_user, menu);
+        inflater.inflate(R.menu.menu_delete_user, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selected = item.getItemId();
+        switch (selected) {
+            case R.id.edit_user:
+                Intent i = new Intent(ShowActivity.this, EditActivity.class);
+                i.putExtra("id", String.valueOf(user.getId()));
+                i.putExtra("username", String.valueOf(user.getName()));
+                i.putExtra("email", String.valueOf(user.getEmail()));
+                i.putExtra("role", String.valueOf(user.getRole()));
+                startActivity(i);
+                break;
+            case R.id.delete_user:
+                newFragment = new DeleteUserDialogFragment();
+                FragmentManager fm = getFragmentManager();
+                newFragment.show(fm, "dialog");
+                break;
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    public void doPositiveClick() {
+        int id = user.getId();
+        apiHandler.deleteUser(pref.getString("token", null), Integer.valueOf(id)).enqueue(deleteUserCallback);
+    }
+
+    public void doNegativeClick() {
+        newFragment.dismiss();
+    }
+
+    Callback<CreateResponse> deleteUserCallback = new Callback<CreateResponse>() {
+        @Override
+        public void onResponse(Call<CreateResponse> call, Response<CreateResponse> response) {
+            if(response.isSuccessful()) {
+                newFragment.dismiss();
+                Intent i = new Intent(ShowActivity.this, ListingActivity.class);
+                startActivity(i);
+                Toast.makeText(getApplicationContext(), "Usuário removido com sucesso", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Não foi possível remover o usuário", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onFailure(Call<CreateResponse> call, Throwable t) {
             t.printStackTrace();
         }
     };
