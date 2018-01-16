9e207b6b875672030eefa49feea49807e65ee3ee
joaoCasella <jpedrocaselladepaula@hotmail.com>
2018-01-16 14:31:11 -0200

    Now the method on the login returns an observable instead of a callback

--- ProjectJoao/app/src/main/java/com/example/android/projectjoao/TaqtileApiHandler.java
+++ ProjectJoao/app/src/main/java/com/example/android/projectjoao/TaqtileApiHandler.java
@@ -1,14 +1,14 @@
 package com.example.android.projectjoao;
 
-import com.example.android.projectjoao.model.CreateResponse;
-import com.example.android.projectjoao.model.ListResponse;
-import com.example.android.projectjoao.model.LoginResponse;
-import com.example.android.projectjoao.model.ShowResponse;
-import com.example.android.projectjoao.model.User;
+import com.example.android.projectjoao.data.models.ListResponse;
+import com.example.android.projectjoao.data.models.LoginResponse;
+import com.example.android.projectjoao.data.models.DefaultResponse;
+import com.example.android.projectjoao.data.models.User;
 
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
@@ -17,25 +17,26 @@ import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
+import rx.Observable;
 
 public interface TaqtileApiHandler {
     String BASE_URL = "http://tq-template-node.herokuapp.com/";
 
     @POST("authenticate")
-    Call<LoginResponse> authenticateUser(@Body User user);
+    Observable<Response<LoginResponse>> authenticateUser(@Body User user);
 
     @GET("users")
     Call<ListResponse> getUsers(@Header("Authorization") String token, @QueryMap Map<String, Integer> pagination);
 
     @GET("user/{id}")
-    Call<ShowResponse> getUser(@Header("Authorization") String token, @Path("id") int id);
+    Call<DefaultResponse> getUser(@Header("Authorization") String token, @Path("id") int id);
 
     @POST("user")
-    Call<CreateResponse> createUser(@Header("Authorization") String token, @Body User user);
+    Call<DefaultResponse> createUser(@Header("Authorization") String token, @Body User user);
 
     @DELETE("user/{id}")
-    Call<CreateResponse> deleteUser(@Header("Authorization") String token, @Path("id") int id);
+    Call<DefaultResponse> deleteUser(@Header("Authorization") String token, @Path("id") int id);
 
     @PUT("user/{id}")
-    Call<CreateResponse> editUser(@Header("Authorization") String token, @Path("id") int id, @Body User user);
+    Call<DefaultResponse> editUser(@Header("Authorization") String token, @Path("id") int id, @Body User user);
 }
